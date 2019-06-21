e92fbc611f284606347b1049189b870ae30912aa
cryptozeny <cryptozeny@gmail.com>
2019-06-22 03:04:52 +0900

    add: seeder-testnet.cryptozeny.com

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -426,6 +426,7 @@ public:
         vSeeds.clear();
         // TODO.SUGAR - seeder
         // nodes with support for servicebits filtering should be at the top
+        vSeeds.emplace_back("seeder-testnet.cryptozeny.com");
         // vSeeds.emplace_back("testnet-seed.bitcoin.jonasschnelli.ch");
         // vSeeds.emplace_back("seed.tbtc.petertodd.org");
         // vSeeds.emplace_back("seed.testnet.bitcoin.sprovoost.nl");
