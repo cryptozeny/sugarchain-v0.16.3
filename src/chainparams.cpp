8a498ae9c2fc4eb0dcb6d81b3ca30feee258cacc
cryptozeny <cryptozeny@gmail.com>
2019-06-23 12:20:18 +0900

    ns

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -426,8 +426,9 @@ public:
         vSeeds.clear();
         // TODO.SUGAR - seeder
         // nodes with support for servicebits filtering should be at the top
-        vSeeds.emplace_back("seed-test.sugarchain.org"); // test
-        vSeeds.emplace_back("dnsseed.sugarchain.ml"); // test
+        vSeeds.emplace_back("seed-test.sugarchain.org");
+        vSeeds.emplace_back("ns.sugarchain.org");
+        vSeeds.emplace_back("dnsseed.sugarchain.ml");
         // vSeeds.emplace_back("testnet-seed.bitcoin.jonasschnelli.ch");
         // vSeeds.emplace_back("seed.tbtc.petertodd.org");
         // vSeeds.emplace_back("seed.testnet.bitcoin.sprovoost.nl");
