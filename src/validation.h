463787588862438c6fb4051da1e29b2f92bebaa6
cryptozeny <cryptozeny@gmail.com>
2019-10-21 05:23:10 +0900

    remove: comment

--- src/validation.h
+++ src/validation.h
@@ -98,13 +98,7 @@ static const int MAX_BLOCKTXN_DEPTH = 10;
  *  Larger windows tolerate larger download speed differences between peer, but increase the potential
  *  degree of disordering of blocks on disk (which make reindexing and pruning harder). We'll probably
  *  want to make this a per-peer adaptive value at some point. */
-
-// FIXME.SUGAR // SURE?
-// 120x faster than bitcoin
-// but 120x is too much
-// 30x faster than LTC
 static const unsigned int BLOCK_DOWNLOAD_WINDOW = 1024;
-// static const unsigned int BLOCK_DOWNLOAD_WINDOW = 1024 * 30; // (was 1024)
 
 /** Time to wait (in seconds) between writing blocks/block index to disk. */
 static const unsigned int DATABASE_WRITE_INTERVAL = 60 * 60;
